56a7a81175c73ba9d5ec03cba7fd7784150e8975
Netero <m_benab@live.concordia.ca>
2025-09-18 23:44:45 -0400

    Create first API request

---
+++ src/main/java/com/mazen/SoftwareEngineerController.java
@@ -0,0 +1,28 @@
+package com.mazen;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/v1/software-engineers")
+public class SoftwareEngineerController {
+
+    @GetMapping
+    public List<SoftwareEngineer> getEngineers() {
+        return List.of(
+                new SoftwareEngineer(
+                        1,
+                        "Mazen",
+                        List.of("JS", "CSS", "Tailwind")
+                ),
+                new SoftwareEngineer(
+                        2,
+                        "Rim",
+                        List.of("Java", "Python", "CSS")
+                )
+        );
+    };
+}
