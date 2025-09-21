e1648053b74c4bd8f306e6c5947a8d826dc191f0
Netero <m_benab@live.concordia.ca>
2025-09-21 15:26:01 -0400

    Create Get reuqest methods for the Topic entity

---
+++ src/main/java/com/mazen/TopicService.java
@@ -0,0 +1,25 @@
+package com.mazen;
+
+import org.springframework.stereotype.Service;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Service
+public class TopicService {
+
+    private List<Topic> topics = Arrays.asList(
+            new Topic("spring", "spring framework", "spring framework description"),
+            new Topic("java", "core java", "core java description"),
+            new Topic("javascript", "javascript", "javascript description")
+    );
+
+    public List<Topic> getAllTopics() {
+        return topics;
+    }
+
+    public Topic getTopic(String id) {
+        return topics.stream().filter(topic -> topic.getId().equals(id)).findFirst().orElse(null);
+    }
+
+}
