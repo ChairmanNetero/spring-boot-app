b54a76c8b709f9cc4ff1879c430e15f09291fdc0
Netero <m_benab@live.concordia.ca>
2025-09-26 23:10:25 -0400

    Add Topic Repository interface and implement CRUD methods using that interface

--- src/main/java/com/mazen/TopicService.java
+++ src/main/java/com/mazen/TopicService.java
@@ -1,46 +1,36 @@
 package com.mazen;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class TopicService {
 
-    private List<Topic> topics = new ArrayList<>(
-            Arrays.asList(
-                    new Topic("spring", "spring framework", "spring framework description"),
-                    new Topic("java", "core java", "core java description"),
-                    new Topic("javascript", "javascript", "javascript description")
-            )
-    );
+    @Autowired
+    private TopicRepository topicRepository;
 
     public List<Topic> getAllTopics() {
-        return topics;
+        return topicRepository.findAll();
     }
 
     public Topic getTopic(String id) {
-        return topics.stream().filter(topic -> topic.getId().equals(id)).findFirst().orElse(null);
+        Optional<Topic> topic = topicRepository.findById(id);
+        return topic.orElse(null);
     }
 
     public void addTopic(Topic topic) {
-        topics.add(topic);
+        topicRepository.save(topic);
     }
 
     public void updateTopic(Topic topic, String id) {
-        for(int i = 0; i < topics.size(); i++) {
-            Topic t =  topics.get(i);
-            if(t.getId().equals(id)) {
-                topics.set(i, topic);
-                return;
-            }
-        }
+        topic.setId(id); // Ensure the topic has the correct ID
+        topicRepository.save(topic); // save() works for both insert and update
     }
 
     public void deleteTopic(String id) {
-        topics.removeIf(topic -> topic.getId().equals(id));
+        topicRepository.deleteById(id);
     }
-
 }
